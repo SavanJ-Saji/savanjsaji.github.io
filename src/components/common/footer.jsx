00975e0bc5e8a3e8f839f50d5c1ca40e57bb082a
Savan J Saji <tve22ae055@cet.ac.in>
2024-08-31 10:47:46 +0530

    footer.jsx

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
 
 				<div className="footer-credits">
 					<div className="footer-credits-text">
-						© 2023 Tharindu.dev. All Rights Reserved.
+						© 2024 Savan J Saji. All Rights Reserved.
 					</div>
 				</div>
 			</div>
