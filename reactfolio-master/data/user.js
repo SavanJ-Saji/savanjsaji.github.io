45bb289f6bbd462a286d39f9b310586013199b6a
Savan J Saji <tve22ae055@cet.ac.in>
2024-08-31 10:12:07 +0530

    user.js

--- reactfolio-master/data/user.js
+++ reactfolio-master/data/user.js
@@ -1,84 +1,81 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		title: "Reactfolio by Savan",
+		name: "Savan J Saji",
+		email: "savanjsaji72@gmail.com",
 		logo: "../logo.png",
 	},
-
 	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		X: "https://X.com/",
+		github: "https://github.com/SavanJ-Saji",
+		linkedin: "https://www.linkedin.com/in/savan-j-saji/",
+		instagram: "https://www.instagram.com/_savan._.j_saji_/",
+		linktree: "https://linktr.ee/savan.j.saji",
+		facebook: "https://www.facebook.com/savan.j.saji"
 	},
-
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Turning Tech Dreams Into Reality",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I'm a skilled web developer passionate about building robust, secure applications. I thrive on tackling complex problems and continuously expanding my technical knowledge. My commitment to best practices and industry standards ensures high-quality code. I'm always eager to embrace new challenges and opportunities to enhance my skills. ",
 	},
-
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Savan J Saji. Welcome to my Web Development Portfolio.",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+			"Hello! My name is Savan J Saji, and I am currently a third-year undergraduate at the College of Engineering Trivandrum, where I am pursuing a Bachelor of Technology in Applied Electronics & Instrumentation.\n\n" +
+			"I am constantly seeking new opportunities to learn and improve in the domains of web development, cyber security & embedded systems. In my leisure time, I enjoy staying up-to-date on advancements in the technology field.\n\n" +
+			"I am very motivated and committed to advancing academically and professionally, and I am always looking for fresh challenges and chances to put what I have learned to work. As I progress through college and beyond, I am excited to keep learning and growing, and I am curious to see where my passion for technology and engineering may lead me.",
 	},
-
 	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		title: "I'm dedicated to innovation and empowering future generations to achieve groundbreaking advancements.",
 		description:
 			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
 	},
-
 	projects: [
 		{
-			title: "Project 1",
+			title: "Random Quote Generator Using HTML, CSS & JavaScript",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+				"Get a fresh perspective with a new quote every time you click the button.",
+			logo: "https://static.wixstatic.com/media/cbed17_d034840f87f34e93ba463891d40581af~mv2.png/v1/fill/w_583,h_305,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cbed17_d034840f87f34e93ba463891d40581af~mv2.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://randomquotegeneratorfiles.on.drv.tw/www.C%2B%2BATMProject.com/quote.html",
 		},
-
 		{
-			title: "Project 2",
+			title: "Tic Tac Toe Game Using HTML,CSS And JavaScript",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+				"Challenge your friends to a game of Tic Tac Toe.",
+			logo: "https://static.wixstatic.com/media/cbed17_e63f87e1267942989a7810780f549b1f~mv2.png/v1/fill/w_543,h_305,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cbed17_e63f87e1267942989a7810780f549b1f~mv2.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://randomquotegeneratorfiles.on.drv.tw/www.tictactoe.com/Tictactoe.html",
 		},
-
 		{
-			title: "Project 3",
+			title: "JOKE GENERATOR USING HTML,CSS And Javascript",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+				"Add some humour to your day with a new joke every time you click.",
+			logo: "https://static.wixstatic.com/media/cbed17_2979702d9de24230a96bb423868be328~mv2.png/v1/fill/w_520,h_293,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cbed17_2979702d9de24230a96bb423868be328~mv2.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://randomquotegeneratorfiles.on.drv.tw/www.Joke.com/Joke.html",
 		},
-
 		{
 			title: "Project 4",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Coming soon",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com",
 		},
-
 		{
 			title: "Project 5",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"On the way",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com",
 		},
 	],
+};
+
+export default INFO;
+
 };
 
 export default INFO;
