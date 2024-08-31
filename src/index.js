68630d2875fb49c652bbc0718c284440062121ce
Savan J Saji <tve22ae055@cet.ac.in>
2024-08-31 06:14:03 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './data/styles.css';
-import "./index.css";
-// import App from "./App";
+import "./data/styles.css"; // Ensure this path is correct
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-function MyApp() {
-  console.log('App component rendered');
-  return (
-	<div className="App">
-	  <h1>Welcome to Savan-Reactfolio</h1>
-	</div>
-  );
-}
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-	<BrowserRouter>
-	<MyApp />
-	</BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
+reportWebVitals();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
