3d4d54b80069e0ab3559ecd0e49077b23d539979
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-01 11:57:53 +0530

    theme-provider.js

--- reactfolio-master/src/theme-provider.js
+++ reactfolio-master/src/theme-provider.js
@@ -1,25 +1,34 @@
 import React, { createContext, useState } from "react";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
 
-// Remove the unused declaration
-
-const defaultContext = {
-  isDarkMode: false,
-  toggleColorMode: () => {},
+export const themes = {
+  light: {
+    background: "#ffffff",
+    color: "#000000",
+  },
+  dark: {
+    background: "#000000",
+    color: "#ffffff",
+  },
 };
 
-export const ThemeContext = createContext(defaultContext);
+export const ThemeContext = createContext({
+  theme: themes.light,
+  toggleTheme: () => {},
+});
 
-export function ThemeProvider({ children }) {
-  const [isDarkMode, setDarkMode] = useState(defaultContext.isDarkMode);
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(themes.light);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === themes.light ? themes.dark : themes.light
+    );
+  };
 
   return (
-    <ThemeContext.Provider
-      value={{
-        isDarkMode,
-        toggleColorMode: () => setDarkMode((isDark) => !isDark),
-      }}
-    >
-      {children}
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <StyledThemeProvider theme={theme}>{children}</StyledThemeProvider>
     </ThemeContext.Provider>
   );
-}
+};
