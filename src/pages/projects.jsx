18a161fbc91320676ee46f7099ebd82450f456c9
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-01 11:15:36 +0530

    projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -39,21 +39,11 @@ const Projects = () => {
 					</div>
 					<div className="projects-container">
 						<div className="title projects-title">
-							Things I’ve made trying to put my dent in the
-							universe.
+							The things I've created to leave my mark on this universe.
 						</div>
 
 						<div className="subtitle projects-subtitle">
-							I've worked on a variety of projects over the years
-							and I'm proud of the progress I've made. Many of
-							these projects are open-source and available for
-							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
+							I've worked on several open-source projects and am excited to share them with the community. Feel free to explore the code, suggest improvements, and collaborate. I'm always open to new ideas and feedback. 
 						</div>
 
 						<div className="projects-list">
