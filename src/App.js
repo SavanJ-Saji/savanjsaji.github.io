e7f7f2dc73fa0e2babd1eca327af5ec3e7998652
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-02 01:59:23 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage';
 import AboutPage from './pages/about';
@@ -7,13 +7,26 @@ import ArticlesPage from './pages/articles';
 import ReadArticlePage from './pages/readArticle';
 import ContactPage from './pages/contact';
 import NotFoundPage from './pages/404';
-import Navbar from './components/common/navBar.jsx'; // Added import statement
+import Navbar from './components/common/navBar.jsx';
+import Toggle from './Toggle.js';
+import { ThemeProvider, ThemeContext } from './theme-provider';
+import './global.css'; // Import global CSS
 
-function App() {
+const AppContent = () => {
+  const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = theme.background;
+    document.body.style.color = theme.color;
+    if (theme === 'dark') {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [theme]);
+
   return (
-    <Router>
-      {/* Added Navbar component */}
-      <Navbar />
+    <div className="content">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
@@ -23,8 +36,20 @@ function App() {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </Router>
+    </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Toggle />
+        <AppContent />
+      </Router>
+    </ThemeProvider>
+  );
+};
 
 export default App;
