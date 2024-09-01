ab8fdf35ef4fd73d7576f1ce280f59dc51b933a2
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-01 11:09:16 +0530

    user.js

--- src/data/user.js
+++ src/data/user.js
@@ -1,72 +1,79 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by Savan",
+		title: "Reactfolio by Savan J Saji",
 		name: "Savan J Saji",
 		email: "savanjsaji72@gmail.com",
-		logo: "../logo.png",
+		logo: "/https://media.licdn.com/dms/image/v2/D5603AQES9tTOqxT_1w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1679919873608?e=1729728000&v=beta&t=ibft9Lv02MY6EowTraUWApM_Hs93GCtU-kXu8J2OsqU",
 	},
+
 	socials: {
-		X: "https://X.com/",
+		X: "https://x.com/Savan_JSaji",
 		github: "https://github.com/SavanJ-Saji",
 		linkedin: "https://www.linkedin.com/in/savan-j-saji/",
 		instagram: "https://www.instagram.com/_savan._.j_saji_/",
 		linktree: "https://linktr.ee/savan.j.saji",
-		facebook: "https://www.facebook.com/savan.j.saji"
+		facebook: "https://www.facebook.com/savan.j.saji/",
 	},
+
 	homepage: {
-		title: "Turning Tech Dreams Into Reality",
+		title: "Turning Tech Dreams into Reality",
 		description:
-			"I'm a skilled web developer passionate about building robust, secure applications. I thrive on tackling complex problems and continuously expanding my technical knowledge. My commitment to best practices and industry standards ensures high-quality code. I'm always eager to embrace new challenges and opportunities to enhance my skills. ",
+			"I'm Savan J Saji. Future engineer, pioneering tomorrow's innovations at College of Engineering Trivandrum.",
 	},
+
 	about: {
-		title: "I’m Savan J Saji. Welcome to my Web Development Portfolio.",
-		description:
-			"Hello! My name is Savan J Saji, and I am currently a third-year undergraduate at the College of Engineering Trivandrum, where I am pursuing a Bachelor of Technology in Applied Electronics & Instrumentation.\n\n" +
-			"I am constantly seeking new opportunities to learn and improve in the domains of web development, cyber security & embedded systems. In my leisure time, I enjoy staying up-to-date on advancements in the technology field.\n\n" +
-			"I am very motivated and committed to advancing academically and professionally, and I am always looking for fresh challenges and chances to put what I have learned to work. As I progress through college and beyond, I am excited to keep learning and growing, and I am curious to see where my passion for technology and engineering may lead me.",
+		title: "I'm Savan J Saji.",
+		description: `I am a third-year undergraduate at the College of Engineering Trivandrum, where I am pursuing a Bachelor of Technology in Applied Electronics & Instrumentation.
+		I am constantly seeking new opportunities to learn and improve in web development, cyber security & embedded systems. In my leisure time, I enjoy staying up-to-date on advancements in the technology field. I am very motivated and committed to advancing academically and professionally, and I am always looking for fresh challenges and chances to put what I have learned to work. As I progress through college and beyond, I am excited to keep learning and growing, and I am curious to see where my passion for technology and engineering may lead me.`,
 	},
+
 	articles: {
-		title: "I'm dedicated to innovation and empowering future generations to achieve groundbreaking advancements.",
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
 		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+			"Chronological collection of my long-form thoughts on programming, tech, product design, and more.",
 	},
+
 	projects: [
 		{
-			title: "Random Quote Generator Using HTML, CSS & JavaScript",
-			description:
-				"Get a fresh perspective with a new quote every time you click the button.",
-			logo: "/https://static.wixstatic.com/media/cbed17_d034840f87f34e93ba463891d40581af~mv2.png/v1/fill/w_583,h_305,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cbed17_d034840f87f34e93ba463891d40581af~mv2.png",
-			linkText: "View Project",
-			link: "https://randomquotegeneratorfiles.on.drv.tw/www.C%2B%2BATMProject.com/quote.html",
-		},
-		{
-			title: "Tic Tac Toe Game Using HTML,CSS And JavaScript",
-			description:
-				"Challenge your friends to a game of Tic Tac Toe.",
-			logo: "/https://static.wixstatic.com/media/cbed17_e63f87e1267942989a7810780f549b1f~mv2.png/v1/fill/w_543,h_305,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cbed17_e63f87e1267942989a7810780f549b1f~mv2.png",
-			linkText: "View Project",
-			link: "https://randomquotegeneratorfiles.on.drv.tw/www.tictactoe.com/Tictactoe.html",
-		},
-		{
-			title: "JOKE GENERATOR USING HTML,CSS And Javascript",
-			description:
-				"Add some humour to your day with a new joke every time you click.",
-			logo: "/https://static.wixstatic.com/media/cbed17_2979702d9de24230a96bb423868be328~mv2.png/v1/fill/w_520,h_293,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cbed17_2979702d9de24230a96bb423868be328~mv2.png",
-			linkText: "View Project",
-			link: "https://randomquotegeneratorfiles.on.drv.tw/www.Joke.com/Joke.html",
-		},
-		{
-			title: "Project 4",
+			title: "Project 1",
 			description:
 				"Coming soon",
 			logo: "/https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com",
 		},
+
+		{
+			title: "Tic Tac Toe Game Using HTML, CSS And JavaScript",
+			description:
+				"Challenge your friends to a game of Tic Tac Toe.",
+			logo: "/https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://randomquotegeneratorfiles.on.drv.tw/www.tictactoe.com/Tictactoe.html",
+		},
+
+		{
+			title: "JOKE GENERATOR USING HTML, CSS And Javascript",
+			description:
+				"Add some humour to your day with a new joke every time you click.",
+			logo: "/https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://randomquotegeneratorfiles.on.drv.tw/www.Joke.com/Joke.html",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Not any time soon",
+			logo: "/https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
 		{
 			title: "Project 5",
 			description:
-				"On the way",
+				"Not any time soon",
 			logo: "/https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com",
@@ -75,6 +82,7 @@ const INFO = {
 };
 
 export default INFO;
+
 
 
 
