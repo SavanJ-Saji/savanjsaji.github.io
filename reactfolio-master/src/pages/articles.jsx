59ded975229ada41fe1b6bff0a08a26a57c13ca6
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-02 22:09:06 +0530

    articles.jsx

--- reactfolio-master/src/pages/articles.jsx
+++ reactfolio-master/src/pages/articles.jsx
@@ -1,28 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 
-import NavBar from "../components/common/navBar";
-import Footer from "../components/common/footer";
+import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faSquareXTwitter,
+	faGithub,
+	faStaylinked,
+	faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 import Logo from "../components/common/logo";
-import Article from "../components/articles/article";
+import Footer from "../components/common/footer";
+import NavBar from "../components/common/navBar";
+import Article from "../components/articles/article"; 
+import Works from "../components/homepage/works";
+import AllProjects from "../components/projects/allProjects";
 
 import INFO from "../data/user";
 import SEO from "../data/seo";
-import myArticles from "../data/articles";
+const myArticles = [article_1 ,article_2 ,article_3]; 
 
-import "./styles/articles.css";
+import "./styles/homepage.css";
+import { article_1, article_2, article_3 } from "../../data/articles";
 
-const Articles = () => {
+const Homepage = () => {
+	const [stayLogo, setStayLogo] = useState(false);
+	const [logoSize, setLogoSize] = useState(80);
+	const [oldLogoSize, setOldLogoSize] = useState(80);
+
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
 
-	const currentSEO = SEO.find((item) => item.page === "articles");
+	useEffect(() => {
+		const handleScroll = () => {
+			let scroll = Math.round(window.pageYOffset, 2);
+
+			let newLogoSize = 80 - (scroll * 4) / 10;
+
+			if (newLogoSize < oldLogoSize) {
+				if (newLogoSize > 40) {
+					setLogoSize(newLogoSize);
+					setOldLogoSize(newLogoSize);
+					setStayLogo(false);
+				} else {
+					setStayLogo(true);
+				}
+			} else {
+				setLogoSize(newLogoSize);
+				setStayLogo(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, [logoSize, oldLogoSize]);
+
+	const currentSEO = SEO.find((item) => item.page === "home");
+
+	const logoStyle = {
+		display: "flex",
+		position: stayLogo ? "fixed" : "relative",
+		top: stayLogo ? "3vh" : "auto",
+		zIndex: 999,
+		border: stayLogo ? "1px solid white" : "none",
+		borderRadius: stayLogo ? "50%" : "none",
+		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+	};
 
 	return (
 		<React.Fragment>
 			<Helmet>
-				<title>{`Articles | ${INFO.main.title}`}</title>
+				<title>{INFO.main.title}</title>
 				<meta name="description" content={currentSEO.description} />
 				<meta
 					name="keywords"
@@ -31,44 +81,122 @@ const Articles = () => {
 			</Helmet>
 
 			<div className="page-content">
-				<NavBar active="articles" />
+				<NavBar active="home" />
 				<div className="content-wrapper">
-					<div className="articles-logo-container">
-						<div className="articles-logo">
-							<Logo width={46} />
+					<div className="homepage-logo-container">
+						<div style={logoStyle}>
+							<Logo width={logoSize} link={false} />
 						</div>
 					</div>
 
-					<div className="articles-main-container">
-						<div className="title articles-title">
-							{INFO.articles.title}
+					<div className="homepage-container">
+						<div className="homepage-first-area">
+							<div className="homepage-first-area-left-side">
+								<div className="title homepage-title">
+									{INFO.homepage.title}
+								</div>
+
+								<div className="subtitle homepage-subtitle">
+									{INFO.homepage.description}
+								</div>
+							</div>
+
+							<div className="homepage-first-area-right-side">
+								<div className="homepage-image-container">
+									<div className="homepage-image-wrapper">
+										<img
+											src="homepage.jpg"
+											alt="about"
+											className="homepage-image"
+										/>
+									</div>
+								</div>
+							</div>
 						</div>
 
-						<div className="subtitle articles-subtitle">
-							{INFO.articles.description}
+						<div className="homepage-socials">
+							<a
+								href={INFO.socials.X}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faSquareXTwitter}
+									className="homepage-social-icon"
+								/>
+							</a>
+							<a
+								href={INFO.socials.github}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faGithub}
+									className="homepage-social-icon"
+								/>
+							</a>
+							<a
+								href={INFO.socials.linktree}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faStaylinked}
+									className="homepage-social-icon"
+								/>
+							</a>
+							<a
+								href={INFO.socials.instagram}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faInstagram}
+									className="homepage-social-icon"
+								/>
+							</a>
+							<a
+								href={`mailto:${INFO.main.email}`}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faMailBulk}
+									className="homepage-social-icon"
+								/>
+							</a>
 						</div>
 
-						<div className="articles-container">
-							<div className="articles-wrapper">
+						<div className="homepage-projects">
+							<AllProjects />
+						</div>
+
+						<div className="homepage-after-title">
+							<div className="homepage-articles">
 								{myArticles.map((article, index) => (
 									<div
-										className="articles-article"
+										className="homepage-article"
 										key={(index + 1).toString()}
 									>
 										<Article
 											key={(index + 1).toString()}
-											date={article().date}
-											title={article().title}
-											description={article().description}
+											date={article.date}
+											title={article.title}
+											description={article.description}
 											link={"/article/" + (index + 1)}
 										/>
 									</div>
 								))}
 							</div>
+
+							<div className="homepage-works">
+								<Works />
+							</div>
 						</div>
-					</div>
-					<div className="page-footer">
-						<Footer />
+
+						<div className="page-footer">
+							<Footer />
+						</div>
 					</div>
 				</div>
 			</div>
@@ -76,4 +204,4 @@ const Articles = () => {
 	);
 };
 
-export default Articles;
+export default Homepage;
