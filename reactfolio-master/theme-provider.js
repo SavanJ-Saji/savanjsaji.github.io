9fae185a7ec39f7e388f159058d8f5fd47758f75
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-03 22:34:51 +0530

    theme-provider.js

---
+++ reactfolio-master/theme-provider.js
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+const themes = {
+  light: {
+    background: '#ffffff',
+    color: '#000000',
+  },
+  dark: {
+    background: '#000000',
+    color: '#ffffff',
+  },
+};
+
+export const ThemeContext = createContext({
+  theme: themes.light,
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(themes.light);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === themes.light ? themes.dark : themes.light
+    );
+  };
+
+  const value = useMemo(() => ({ theme, toggleTheme }), [theme]);
+
+  return (
+    <ThemeContext.Provider value={value}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
