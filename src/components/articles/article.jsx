195507657469e962800350730bd2066d50d7163f
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-03 13:40:12 +0530

    article.jsx

--- src/components/articles/article.jsx
+++ src/components/articles/article.jsx
@@ -1,36 +1,40 @@
 import React from "react";
+import PropTypes from 'prop-types'; // Import PropTypes for prop validation
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 import "./style/article.css";
 
-const Article = (props) => {
-	const { date, title, description, link } = props;
-
+const Article = ({ date, title, description, link }) => {
 	return (
-		<React.Fragment>
-			<div className="article">
-				<div className="article-left-side">
-					<div className="article-date">{date}</div>
-				</div>
-
-				<Link to={link}>
-					<div className="article-right-side">
-						<div className="article-title">{title}</div>
-						<div className="article-description">{description}</div>
-						<div className="article-link">
-							Read Article{" "}
-							<FontAwesomeIcon
-								style={{ fontSize: "10px" }}
-								icon={faChevronRight}
-							/>
-						</div>
-					</div>
-				</Link>
+		<div className="article">
+			<div className="article-left-side">
+				<div className="article-date">{date}</div>
 			</div>
-		</React.Fragment>
+
+			<Link to={link}>
+				<div className="article-right-side">
+					<div className="article-title">{title}</div>
+					<div className="article-description">{description}</div>
+					<div className="article-link">
+						Read Article{" "}
+						<FontAwesomeIcon
+							style={{ fontSize: "10px" }}
+							icon={faChevronRight}
+						/>
+					</div>
+				</div>
+			</Link>
+		</div>
 	);
+};
+
+Article.propTypes = {
+	date: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
 };
 
 export default Article;
