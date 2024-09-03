2afbe00eb5947d45196cfc3bff0679e663461138
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-03 15:08:54 +0530

    articles.js

--- src/data/articles.js
+++ src/data/articles.js
@@ -8,42 +8,6 @@ import AllProjects from "../components/projects/allProjects";
 import INFO from "../data/user";
 import SEO from "../data/seo";
 
-const Article = () => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
-
-    const currentSEO = SEO.find((item) => item.page === "projects");
-
-    // Use the custom hook
-    const [ref, isVisible] = useIntersectionObserver({ threshold: 0.1 });
-
-    return (
-        <React.Fragment>
-            <Helmet>
-                <title>{`Projects | ${INFO.main.title}`}</title>
-                <meta name="description" content={currentSEO.description} />
-                <meta name="keywords" content={currentSEO.keywords.join(", ")} />
-            </Helmet>
-
-            <div className="page-content">
-                <NavBar />
-                <div className="content-wrapper">
-                    <div ref={ref} className={`logo-container ${isVisible ? 'visible' : ''}`}>
-                        <Logo />
-                    </div>
-                    <div className="projects-container">
-                        <AllProjects />
-                    </div>
-                    <div className="page-footer">
-                        <Footer />
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
-    );
-}
-
 const ArticleComponent = ({ article }) => {
     const [ref, isVisible] = useIntersectionObserver({ threshold: 0.1 });
 
@@ -58,57 +22,57 @@ const ArticleComponent = ({ article }) => {
     );
 }
 
-const article_1 = {
-    date: "7 May 2023",
-    title: "The Benefits of Cloud Computing",
-    description: "Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
-    keywords: ["The Benefits of Cloud Computing", "Savan", "Savan J", "Savan J Saji"],
-    style: `
-        .article-content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
+const articles = [
+    {
+        date: "7 May 2023",
+        title: "The Benefits of Cloud Computing",
+        description: "Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
+        keywords: ["The Benefits of Cloud Computing", "Savan", "Savan J", "Savan J Saji"],
+        style: `
+            .article-content {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
 
-        .randImage {
-            align-self: center;
-            outline: 2px solid red;
-        }
-    `,
-    body: (
-        <React.Fragment>
-            <div className="article-content">
-                <div className="paragraph">Content of article 1</div>
-                <img src="https://picsum.photos/200/300" alt="random" className="randImage" />
-            </div>
-        </React.Fragment>
-    )
-};
+            .randImage {
+                align-self: center;
+                outline: 2px solid red;
+            }
+        `,
+        body: (
+            <React.Fragment>
+                <div className="article-content">
+                    <div className="paragraph">Content of article 1</div>
+                    <img src="https://picsum.photos/200/300" alt="random" className="randImage" />
+                </div>
+            </React.Fragment>
+        )
+    },
+    {
+        title: "Beyond Chatbots: Exploring the Limitless Possibilities of OpenAI's ChatGPT and GPT-4",
+        author: "Savan J Saji",
+        link: "https://brucewaynebatman77.wixsite.com/savan-j-saji/post/beyond-chatbots-exploring-the-limitless-possibilities-of-openai-s-chatgpt-and-gpt-4",
+        body: (
+            <React.Fragment>
+                <h1>Beyond Chatbots: Exploring the Limitless Possibilities of OpenAI's ChatGPT and GPT-4</h1>
+                <p>Read the full article <a href="https://brucewaynebatman77.wixsite.com/savan-j-saji/post/beyond-chatbots-exploring-the-limitless-possibilities-of-openai-s-chatgpt-and-gpt-4">here</a></p>
+            </React.Fragment>
+        ),
+    },
+    {
+        date: "7 May 2023",
+        title: "Artificial Intelligence in Healthcare",
+        description: "AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+        keywords: ["Artificial Intelligence in Healthcare", "Savan", "Savan J", "Savan J Saji"],
+        style: ``,
+        body: (
+            <React.Fragment>
+                <h1>Content of article 2</h1>
+            </React.Fragment>
+        )
+    }
+];
 
-const article_2 = {
-    title: "Beyond Chatbots: Exploring the Limitless Possibilities of OpenAI's ChatGPT and GPT-4",
-    author: "Savan J Saji",
-    link: "https://brucewaynebatman77.wixsite.com/savan-j-saji/post/beyond-chatbots-exploring-the-limitless-possibilities-of-openai-s-chatgpt-and-gpt-4",
-    body: (
-        <React.Fragment>
-            <h1>Beyond Chatbots: Exploring the Limitless Possibilities of OpenAI's ChatGPT and GPT-4</h1>
-            <p>Read the full article <a href="https://brucewaynebatman77.wixsite.com/savan-j-saji/post/beyond-chatbots-exploring-the-limitless-possibilities-of-openai-s-chatgpt-and-gpt-4">here</a></p>
-        </React.Fragment>
-    ),
-};
-
-const article_3 = {
-    date: "7 May 2023",
-    title: "Artificial Intelligence in Healthcare",
-    description: "AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
-    keywords: ["Artificial Intelligence in Healthcare", "Savan", "Savan J", "Savan J Saji"],
-    style: ``,
-    body: (
-        <React.Fragment>
-            <h1>Content of article 2</h1>
-        </React.Fragment>
-    )
-};
-
-export { Article, article_1, article_2, article_3, ArticleComponent };
-export default { article_1, article_2, article_3 };
+export { ArticleComponent, articles };
+export default articles;
