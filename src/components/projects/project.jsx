c3d8e20afd4c9d18e69fd4ba435acc37584bacc5
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-04 10:26:38 +0530

    project.jsx

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,35 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles/project.css";
 
-const Project = (props) => {
-	const { logo, title, description, linkText, link } = props;
-
-	return (
-		<React.Fragment>
-			<div className="project">
-				<Link to={link}>
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
-
-							<div className="project-link-text">{linkText}</div>
-						</div>
+const Project = ({ logo, title, description, linkText, link }) => (
+	<React.Fragment>
+		<div className="project">
+			<Link to={link}>
+				<div className="project-container">
+					<div className="project-logo">
+						<img src={logo} alt={`${title} logo`} />
 					</div>
-				</Link>
-			</div>
-		</React.Fragment>
-	);
+					<div className="project-title">{title}</div>
+					<div className="project-description">{description}</div>
+					<div className="project-link">
+						<div className="project-link-icon">
+							<FontAwesomeIcon icon={faLink} />
+						</div>
+						<div className="project-link-text">{linkText}</div>
+					</div>
+				</div>
+			</Link>
+		</div>
+	</React.Fragment>
+);
+
+Project.propTypes = {
+	logo: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	linkText: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
 };
 
 export default Project;
