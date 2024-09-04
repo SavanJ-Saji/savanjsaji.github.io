a05ad23fbf7ddba3369bf6aacc16ff7ec5aef54a
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-04 10:51:17 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initGA, logPageView } from './analytics';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route component={App} />
+    </Router>
   </React.StrictMode>
 );
+
+// Initialize Google Analytics
+initGA();
+
+const Main = () => {
+    useEffect(() => {
+        logPageView();
+    }, []);
+
+    return <App />;
+};
 
 // Measure performance only in development mode
 if (process.env.NODE_ENV === 'development') {
