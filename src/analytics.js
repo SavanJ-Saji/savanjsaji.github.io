04da01310061ea52c234a003aa6eee8390749098
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-04 11:01:56 +0530

    analytics.js

--- src/analytics.js
+++ src/analytics.js
@@ -1,6 +1,11 @@
 import { Analytics } from '@vercel/analytics/react';
- 
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = "SGPRFB29JG"; // Replace with your Google Analytics tracking ID
+
 export default function RootLayout({ children }) {
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <html lang="en">
       <head>
