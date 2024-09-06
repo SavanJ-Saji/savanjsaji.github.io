32596a5b8f37d0407b132e85011d427a3f1cf4ae
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-06 13:28:37 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RootLayout from './analytics'; // Ensure correct import path
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RootLayout>
+      <App />
+    </RootLayout>
   </React.StrictMode>
 );
 
-reportWebVitals();
+reportWebVitals();;
