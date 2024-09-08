962a85a1e3020d977511b0d87e65b39ed3de4d38
Savan J Saji <tve22ae055@cet.ac.in>
2024-09-08 21:58:56 +0530

    seo.js

--- reactfolio-master/data/seo.js
+++ reactfolio-master/data/seo.js
@@ -2,36 +2,36 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"Turning Tech Dreams into Reality.",
+		keywords: ["Savan", "Savan J", "Savan J Saji"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"Hello! My name is Savan J Saji, and I am currently a third-year undergraduate at the College of Engineering Trivandrum, where I am pursuing a Bachelor of Technology in Applied Electronics & Instrumentation. I am constantly seeking new opportunities to learn and improve in the domains of web development, cyber security & embedded systems. In my leisure time, I enjoy staying up-to-date on advancements in the technology field. I am very motivated and committed to advancing both academically and professionally, and I am always looking for fresh challenges and chances to put what I have learned to work. As I progress through college and beyond, I am excited to keep learning and growing, and I am curious to see where my passion for technology and engineering may lead me..",
+		keywords: ["Savan", "Savan J", "Savan J Saji"],
 	},
 
 	{
 		page: "articles",
 		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"Chronological collection of my long-form thoughts on programming, tech, product design, and more.",
+		keywords: ["Savan", "Savan J", "Savan J Saji"],
 	},
 
 	{
 		page: "projects",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I've gained significant experience working on diverse projects, many of which are open-source and available for community collaboration.  .",
+		keywords: ["Savan", "Savan J", "Savan J Saji"],
 	},
 
 	{
 		page: "contact",
 		description:
-			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I'm eager to collaborate on exciting new projects.  Let's connect and explore possibilities! ",
+		keywords: ["Savan", "Savan J", "Savan J Saji"],
 	},
 ];
 
